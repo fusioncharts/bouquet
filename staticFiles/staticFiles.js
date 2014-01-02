772194e2e92378352b2c355f49ecd397a7cd308e
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2014-01-02 14:56:10 +0530

    Refactor staticFiles.js and improve comments

--- staticFiles/staticFiles.js
+++ staticFiles/staticFiles.js
@@ -4,15 +4,17 @@
  */
 'use strict';
 
-var fs = require('jsdoc/fs'),
+var PATH_SEPARATOR = ":",
+    fs = require('jsdoc/fs'),
     pathUtil = require('jsdoc/path'),
     conf = env.conf.staticFiles || {},
     filter = new (require('jsdoc/src/filter')).Filter(conf),
     scanner = new (require('jsdoc/src/scanner')).Scanner(),
     outDir = env.opts.destination,
-    SLASH = pathUtil.sep,
-    RECURSIVE = conf.recursive || false,
-    RECURSIVE_LEVEL = conf.recursiveLevel || (!RECURSIVE ? 1 : 3);
+    // `recursive` will be false by default
+    recursive = conf.recursive || false,
+    // By default recursion will go three levels deep if `recursive` is `true`
+    recursiveLevel = conf.recursiveLevel || (!recursive ? 1 : 3);
 
 /**
  * Splits include paths into an array of source and destination paths
@@ -20,25 +22,31 @@ var fs = require('jsdoc/fs'),
  * @example
  * ["images:tutorials/images", "js"]
  * @param {array} paths - Array of items in the format `"destination:source"` or, simply, `"source"`
- * @returns {array} An array containing relative source and destination paths in the format `{dest: "", source: ""}`
+ * @returns {array} An array containing relative source and destination paths in the format `{source: "", destination: ""}`
  */
 var splitInclude = function (paths) {
     var output = [];
-    paths.forEach( function(path) {
-        // Test if path contains a colon
-        if ((/:/).test(path)) {
-            // If it contains a colon, split it into two parts based on the colon
-            var split = path.split(":");
-            // Add an object with the first part of the split path as destination
-            // and the second as source
-            output.push({ dest: split[0], source: split[1] });
-        } else {
-            // If path does not contain a colon, destination and source will be same
-            output.push({ dest: path, source: path });
-        }
+
+    // Ensure that `paths` is an array and perform action on each of the array items
+    paths && paths.forEach && paths.forEach(function (path) {
+        // Tests that `path` is a string or an object with `match` property which takes a regex.
+        // The regex splits `path` based on the presence of a colon within the string.
+
+        // The `match()` method returns an array in which the first item is the string being matched,
+        // the second item is the destination and the third item is the source.
+        // If user provides a string without colons, then the entire path becomes the source, and
+        // the destination becomes the same as source. i.e. the same folder structure is created 
+        // in the destination.
+        (path = path && path.match && path.match(/^([^\:]*?)\:?([^\:]+)$/)) && output.push({
+            source: path[2],
+            // If no colons were given, destination remains an empty string. So we make destination
+            //same as source
+            destination: path[1] || path[2]
+        });
     });
+
     return output;
-}
+};
 
 exports.handlers = {
 
@@ -59,9 +67,9 @@ exports.handlers = {
             // Iterate over each `include` paths and perform operation
             includes.forEach( function(elem) {
                 // Prepare path to destination
-                destPath = pathUtil.join(outDir, elem.dest) + SLASH;
+                destPath = pathUtil.join(outDir, elem.destination) + pathUtil.sep;
                 // Scan the source path and filter inclusions and exclusions
-                filteredList = scanner.scan([elem.source], RECURSIVE_LEVEL, filter);
+                filteredList = scanner.scan([elem.source], recursiveLevel, filter);
                 // Iterate over each of the filtered paths available and copy them to destination
                 filteredList.forEach( function(currentPath) {
                     // Generate source path
@@ -77,4 +85,4 @@ exports.handlers = {
             });
         }
     }
-}
+};

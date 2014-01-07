9917f46c3efbaf3dd56c499d5ab4b44d53631fd7
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2014-01-08 01:16:52 +0530

    Add very basic specs for staticFiles and improved staticFiles.conf

--- test/staticFiles-spec.js
+++ test/staticFiles-spec.js
@@ -1,5 +1,63 @@
+var fs = require("fs"),
+    CONFFILE = "./test/staticFiles.conf",
+    SRCDIR = "test/fixtures/staticFiles",
+    OUTDIR = "tmp/out-staticFiles",
+    config;
+
+/**
+ * The test below is left here for historical reasons
+ */
 describe ("blank", function () {
     it ("must be blank", function () {
         expect("blank").toBe("blank");
     });
+});
+
+/**
+ * Test staticFiles conf
+ */
+describe ("staticFilesConf", function () {
+
+    it ("must exist", function () {
+        expect(fs.existsSync(CONFFILE)).toBeTruthy();
+    });
+
+    it ("must valid JSON", function () {
+        expect(config = JSON.parse(fs.readFileSync(CONFFILE).toString())).toBeTruthy();
+    });
+
+    it ("must have staticFiles and staticFiles.include", function () {
+        expect(config.staticFiles && config.staticFiles.include).toBeTruthy();
+    });
+
+});
+
+/**
+ * Test input
+ */
+describe ("input", function () {
+
+    it ("must exist", function () {
+        expect(fs.existsSync(SRCDIR)).toBeTruthy();
+    });
+
+    it ("must be a directory", function () {
+        expect(fs.statSync(SRCDIR).isDirectory()).toBeTruthy();
+    });
+
+});
+
+/**
+ * Test output
+ */
+describe ("output", function () {
+
+    it ("must exist", function () {
+        expect(fs.existsSync(OUTDIR)).toBeTruthy();
+    });
+
+    it ("must be a directory", function () {
+        expect(fs.statSync(OUTDIR).isDirectory()).toBeTruthy();
+    });
+
 });
